67fccb66e43d32a1f234f7f79c0020427b493261
Greg Glinoga <46800626+gglinoga@users.noreply.github.com>
2019-11-01 15:20:50 -0700

    contact form fixed

--- js/main.js
+++ js/main.js
@@ -149,72 +149,72 @@ $(function() {
 
 
     /*========Contact Form Setup========*/
-    $('#contact-form').on('submit', function(e) {
-        e.preventDefault();
-        var uri = $(this).attr('action');
-        $('#form-submit').val('Wait...');
-        var name = $('#contact-name').val(),
-            email = $('#contact-email').val(),
-            message = $('#contact-message').val();
-        var required = 0;
-        $('.con-validate', this).each(function() {
-            if ($(this).val() == '') {
-                $(this).addClass('con-error');
-                required += 1;
-            } else {
-                if ($(this).hasClass('con-error')) {
-                    $(this).removeClass('con-error');
-                    if (required > 0) {
-                        required -= 1;
-                    }
-                }
-            }
-        });
-        if (required === 0) {
-            $.ajax({
-                type: "POST",
-                url: 'mail.php',
-                data: {
-                    con_name: name,
-                    con_email: email,
-                    con_message: message
-                },
-                success: function(data) {
-                    $("#contact-form input, #contact-form textarea").val('');
-                    $("#contact-submit.primary-button span").html('Done!');
-                    $("#contact-submit.primary-button").addClass("ok");
-                    showAlertBox('success', "Thank You! Your message has been sent.");
-                },
-                error: function(jqXHR, e) {
-                    $('#contact-submit.primary-button span').html('Failed!');
-                    showAlertBox('error', "There was a problem with your submission, please try again.");
-                }
-            });
-        } else {
-            console.log("Validation Error");
-        }
-    })
-    $(".con-validate").keyup(function() {
-        $(this).removeClass('con-error');
-    });
+//     $('#contact-form').on('submit', function(e) {
+//         e.preventDefault();
+//         var uri = $(this).attr('action');
+//         $('#form-submit').val('Wait...');
+//         var name = $('#contact-name').val(),
+//             email = $('#contact-email').val(),
+//             message = $('#contact-message').val();
+//         var required = 0;
+//         $('.con-validate', this).each(function() {
+//             if ($(this).val() == '') {
+//                 $(this).addClass('con-error');
+//                 required += 1;
+//             } else {
+//                 if ($(this).hasClass('con-error')) {
+//                     $(this).removeClass('con-error');
+//                     if (required > 0) {
+//                         required -= 1;
+//                     }
+//                 }
+//             }
+//         });
+//         if (required === 0) {
+//             $.ajax({
+//                 type: "POST",
+//                 url: 'mail.php',
+//                 data: {
+//                     con_name: name,
+//                     con_email: email,
+//                     con_message: message
+//                 },
+//                 success: function(data) {
+//                     $("#contact-form input, #contact-form textarea").val('');
+//                     $("#contact-submit.primary-button span").html('Done!');
+//                     $("#contact-submit.primary-button").addClass("ok");
+//                     showAlertBox('success', "Thank You! Your message has been sent.");
+//                 },
+//                 error: function(jqXHR, e) {
+//                     $('#contact-submit.primary-button span').html('Failed!');
+//                     showAlertBox('error', "There was a problem with your submission, please try again.");
+//                 }
+//             });
+//         } else {
+//             console.log("Validation Error");
+//         }
+//     })
+//     $(".con-validate").keyup(function() {
+//         $(this).removeClass('con-error');
+//     });
 
-    /********** Adding Alert Box **********/
-    $('#contact-submit').before('<div class="alert-container"></div>');
+//     /********** Adding Alert Box **********/
+//     $('#contact-submit').before('<div class="alert-container"></div>');
 
-    /********** Function Show Alert Box **********/
-    function showAlertBox(response, message) {
+//     /********** Function Show Alert Box **********/
+//     function showAlertBox(response, message) {
 
 
-        var $alertBox = $('<div class="alert"></div>'),
-            $alContainer = $('#contact-form .alert-container');
-        if (response == 'success') {
-            $alertBox.addClass('alert-success').html(message);
-            $alContainer.html($alertBox);
-        } else {
-            $alertBox.addClass('alert-danger').html(message);
-            $alContainer.html($alertBox);
-        }
-        $alContainer.fadeIn(300).delay(2000).fadeOut(400);
-    }
+//         var $alertBox = $('<div class="alert"></div>'),
+//             $alContainer = $('#contact-form .alert-container');
+//         if (response == 'success') {
+//             $alertBox.addClass('alert-success').html(message);
+//             $alContainer.html($alertBox);
+//         } else {
+//             $alertBox.addClass('alert-danger').html(message);
+//             $alContainer.html($alertBox);
+//         }
+//         $alContainer.fadeIn(300).delay(2000).fadeOut(400);
+//     }
 
 });
